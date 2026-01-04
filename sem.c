8bed9ddcb4f1bac0332def7152d26976005a138c
Frenky777 <fpakos5@gmail.com>
2026-01-04 13:43:12 +0100

    funkcje pomocnicze do semaforow

--- sem.c
+++ sem.c
@@ -1,1 +1,22 @@
 #include "header.h"
+
+// Funkcja P (Czekaj)
+void sem_p(int semid, int sem_num) {
+    struct sembuf bufor;
+    bufor.sem_num = sem_num;
+    bufor.sem_op = -1; //-1
+    bufor.sem_flg = 0;
+    
+}
+
+// Funkcja V (Zwolnij)
+void sem_v(int semid, int sem_num) {
+    struct sembuf bufor;
+    bufor.sem_num = sem_num;
+    bufor.sem_op = 1; //+1
+    bufor.sem_flg = 0;
+    
+    if (semop(semid, &bufor, 1) == -1) {
+        perror("Blad sem_v");
+    }
+}
